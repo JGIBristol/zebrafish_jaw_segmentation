793fc8f8cffed1997167b522eabe40c65fa491e8
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-09-06 15:59:20 +0100

    script to plot hyperparam tuning results

--- scripts/README.md
+++ scripts/README.md
@@ -3,10 +3,21 @@ Scripts
 Some scripts
 
 
-Meshing
+Setup
 ----
-`mesh.py`: example showing the conversion from tiff (which is what the segmentation model creates) to a mesh
 `create_dicoms.py`: create DICOM files for each CT scan - label pair
 `plot_dicoms.py`: create plots visualising these DICOM files
+
+Training Models
+----
 `train_model.py`: train the model
+
+Hyperparameter Tuning
+----
 `explore_hyperparams.py`: train lots of models with different hyperparameters, to see what's best
+`plot_hyperparams.py`: plot the result of the hyperparameter tuning
+
+Other Stuff
+----
+`mesh.py`: example showing the conversion from tiff (which is what the segmentation model creates) to a mesh
+    [ ] TODO make this useful
---
+++ scripts/plot_hyperparams.py
@@ -0,0 +1,70 @@
+"""
+Plot the results from the hyperparam search
+
+"""
+
+import pathlib
+import argparse
+
+import yaml
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+def _plot_coarse():
+    """
+    Read the losses and indices from the coarse search,
+    then plot them colour coded by LR
+
+    """
+    paths = sorted(
+        list((pathlib.Path(__file__).parents[1] / "tuning_output" / "coarse").glob("*"))
+    )
+
+    fig, axes = plt.subplots(1, 2)
+    cmap = plt.get_cmap("viridis")
+
+    for path in tqdm(paths):
+        # Get the training and validation loss from each
+        # Average over batches
+        train_loss = np.load(path / "train_losses.npy").mean(axis=1)
+        val_loss = np.load(path / "val_losses.npy").mean(axis=1)
+
+        # Get the LR
+        with open(path / "config.yaml") as f:
+            params = yaml.safe_load(f)
+        lr = params["learning_rate"]
+
+        # Scale to between 0 and 1
+        scaled_lr = (np.log10(lr) + 6) / 7
+
+        # Get the colour from the LR
+        colour = cmap(scaled_lr)
+
+        axes[0].plot(train_loss, color=colour)
+        axes[1].plot(val_loss, color=colour)
+
+    fig.tight_layout()
+
+    fig.savefig("coarse_search.png")
+
+
+def main(mode: str):
+    if mode == "coarse":
+        _plot_coarse()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Plot the results of hyperparameter search"
+    )
+
+    parser.add_argument(
+        "mode",
+        type=str,
+        choices={"coarse", "med", "fine"},
+        help="Granularity of the search.",
+    )
+
+    main(**vars(parser.parse_args()))
