edb715893e202f936a97b5f3d413177970f4b11b
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-10-21 16:06:48 +0100

    plot the training data

--- scripts/plot_train_data.py
+++ scripts/plot_train_data.py
@@ -3,7 +3,17 @@ Plot the training data - to e.g. visualize the transforms
 
 """
 
+import pathlib
 import argparse
+
+import torch
+import numpy as np
+import torchio as tio
+import matplotlib.pyplot as plt
+
+from fishjaw.util import util
+from fishjaw.model import data
+from fishjaw.visualisation import images_3d
 
 
 def main(*, step: int, epochs: int):
@@ -11,6 +21,37 @@ def main(*, step: int, epochs: int):
     Read the DICOMs from disk () Create the data config
 
     """
+    # Create training config
+    config = util.userconf()
+    torch.manual_seed(config["torch_seed"])
+    rng = np.random.default_rng(seed=config["test_train_seed"])
+
+    train_subjects, val_subjects, _ = data.read_dicoms_from_disk(config, rng)
+    data_config = data.DataConfig(config, train_subjects, val_subjects)
+
+    output_dir = (
+        pathlib.Path(__file__).parents[1]
+        / util.config()["script_output"]
+        / "train_data"
+    )
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    # Epochs
+    for epoch in range(0, epochs, step):
+        # Batches
+        for i, batch in enumerate(data_config.train_data):
+            images = batch[tio.IMAGE][tio.DATA]
+            masks = batch[tio.LABEL][tio.DATA]
+            # Images per batch
+            for j, (image, mask) in enumerate(zip(images, masks)):
+                out_path = str(
+                    output_dir / f"traindata_epoch_{epoch}_batch_{i}_img_{j}.png"
+                )
+
+                fig, _ = images_3d.plot_slices(image.squeeze().numpy(), mask.squeeze().numpy())
+                fig.savefig(out_path)
+                plt.close(fig)
 
 
 if __name__ == "__main__":
