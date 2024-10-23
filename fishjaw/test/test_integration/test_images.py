15879040b23fe3702692d8d2de9f98d865af486b
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-10-23 12:46:10 +0100

    tests for reading stuff from the CSV

---
+++ fishjaw/test/test_integration/test_images.py
@@ -0,0 +1,23 @@
+"""
+Tests for stuff in the images.py directory
+
+"""
+from ...images import transform
+
+def test_around_centre():
+    """
+    Check that we can properly read the bool from the csv
+
+    """
+    assert transform.around_centre(30)
+    assert transform.around_centre(107)
+
+    # TODO add one for the false ones
+
+
+def test_find_coords():
+    """
+    Check that we can properly read the co-ords from the csv
+
+    """
+    assert transform.centre(30) == (1435, 161, 390)
