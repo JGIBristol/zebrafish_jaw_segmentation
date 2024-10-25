f66decd1692f14798d312ad00b2671c37cce1f4f
Richard Lane <richard.lane@bristol.ac.uk>
2024-10-25 17:48:18 +0100

    fix indent

--- scripts/find_rear_jaw_centres.py
+++ scripts/find_rear_jaw_centres.py
@@ -36,36 +36,36 @@ def main():
     Read the DICOMs, find the last slice that contains labels, then find the XY location of the labels
 
     """
-plot_dir = pathlib.Path("tmp/")
-if not plot_dir.is_dir():
-    plot_dir.mkdir()
+    plot_dir = pathlib.Path("tmp/")
+    if not plot_dir.is_dir():
+        plot_dir.mkdir()
 
-    folder = pathlib.Path("dicoms/Training set 3 (base of jaw)")
-    for path in folder.glob("*.dcm"):
-        # Open each DICOM in the training set 3 folder
-        _, mask = io.read_dicom(path)
+        folder = pathlib.Path("dicoms/Training set 3 (base of jaw)")
+        for path in folder.glob("*.dcm"):
+            # Open each DICOM in the training set 3 folder
+            _, mask = io.read_dicom(path)
 
-        # Find the last Z slice for which there are at least some nonzero values
-        n_required = 3
-        z_nonzero = np.sum(mask, axis=(1, 2)) > n_required
-        idx = len(z_nonzero) - np.argmax(z_nonzero[::-1])
+            # Find the last Z slice for which there are at least some nonzero values
+            n_required = 3
+            z_nonzero = np.sum(mask, axis=(1, 2)) > n_required
+            idx = len(z_nonzero) - np.argmax(z_nonzero[::-1])
 
-        # Find the xy centre of that slice
-        x, y = find_xy(mask[idx - 1])
+            # Find the xy centre of that slice
+            x, y = find_xy(mask[idx - 1])
 
-        # Find n from the path
-        n = int(path.stem.split("_", maxsplit=1)[-1])
+            # Find n from the path
+            n = int(path.stem.split("_", maxsplit=1)[-1])
 
-        # Plot them
-        fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-        for i, (axis, img) in enumerate(zip(axes, mask[idx - 2 : idx + 1])):
-            axis.imshow(img)
-            axis.plot(y, x, "ro")
-        fig.savefig(plot_dir / f"{n}.png")
-        plt.close(fig)
+            # Plot them
+            fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+            for i, (axis, img) in enumerate(zip(axes, mask[idx - 2 : idx + 1])):
+                axis.imshow(img)
+                axis.plot(y, x, "ro")
+            fig.savefig(plot_dir / f"{n}.png")
+            plt.close(fig)
 
-        # Output n,z
-        print(n, idx, round(x), round(y), "FALSE", sep=",")
+            # Output n,z
+            print(n, idx, round(x), round(y), "FALSE", sep=",")
 
 if __name__ == "__main__":
     main()
