d15610c486f30e6c30609e2f4e1de8c0cd51256c
richard-lane <56296326+richard-lane@users.noreply.github.com>
2024-10-25 10:20:21 +0100

    Tests for cropping code (#14)
    Tests for cropping code and working implementation for the cropping with an odd window size. Still not entirely clear in my head which slice we're cropping to/from, but that should all get cleared up in the next PR
    ---------
    
    Co-authored-by: Richard Lane <mh19137@it090783.users.bris.ac.uk>

--- fishjaw/test/test_integration/test_images.py
+++ fishjaw/test/test_integration/test_images.py
@@ -3,6 +3,9 @@ Tests for stuff in the images.py directory
 
 """
 
+import pytest
+import numpy as np
+
 from ...images import transform
 
 
@@ -37,3 +40,84 @@ def test_find_coords():
 
     """
     assert transform.centre(30) == (1435, 161, 390)
+
+
+@pytest.fixture(name="test_img")
+def uniform_slices():
+    """
+    Fixture that provides a 10x10 3D array where each slice has uniform values.
+    The first slice is all ones , the second slice is all twos, etc.
+    """
+    return np.arange(1, 11)[:, np.newaxis, np.newaxis] * np.ones(
+        (10, 10, 10), dtype=int
+    )
+
+
+def test_central_crop_even_window(test_img: np.ndarray):
+    """
+    Check that we can correctly crop an image around the centre
+
+    """
+    centre = (5, 5, 5)
+    crop_size = (4, 4, 4)
+
+    # Expected output is an array with values 4, 5, 6, 7
+    expected = np.arange(4, 8)[:, np.newaxis, np.newaxis] * np.ones(
+        crop_size, dtype=int
+    )
+
+    cropped = transform.crop(test_img, centre, crop_size, centred=True)
+
+    assert (cropped == expected).all()
+
+
+def test_central_crop_odd_window(test_img: np.ndarray):
+    """
+    Check that we can correctly crop an image around the centre
+
+    """
+    centre = (5, 5, 5)
+    crop_size = (3, 3, 3)
+
+    # Expected output is an array with values 4, 5, 6
+    expected = np.arange(4, 7)[:, np.newaxis, np.newaxis] * np.ones(
+        crop_size, dtype=int
+    )
+
+    cropped = transform.crop(test_img, centre, crop_size, centred=True)
+
+    assert (cropped == expected).all()
+
+
+def test_offset_crop_even_window(test_img: np.ndarray):
+    """
+    Check that we can correctly crop an image up to a given slice
+
+    """
+    centre = (5, 5, 5)
+    crop_size = (4, 4, 4)
+
+    expected = np.arange(2, 6)[:, np.newaxis, np.newaxis] * np.ones(
+        crop_size, dtype=int
+    )
+
+    cropped = transform.crop(test_img, centre, crop_size, centred=False)
+
+    assert (cropped == expected).all()
+
+
+def test_offset_crop_odd_window(test_img: np.ndarray):
+    """
+    Check that we can correctly crop an image up to a given slice
+
+    """
+    centre = (5, 5, 5)
+    crop_size = (3, 3, 3)
+
+    expected = np.arange(3, 6)[:, np.newaxis, np.newaxis] * np.ones(
+        crop_size, dtype=int
+    )
+
+    cropped = transform.crop(test_img, centre, crop_size, centred=False)
+
+    assert (cropped == expected).all()
