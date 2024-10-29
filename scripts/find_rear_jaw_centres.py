9138eac36c65f77db1b65b9aec096a9f136609c4
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-10-29 16:36:28 +0000

    fix indent

--- scripts/find_rear_jaw_centres.py
+++ scripts/find_rear_jaw_centres.py
@@ -52,91 +52,92 @@ def main():
     if not plot_dir.is_dir():
         plot_dir.mkdir()
 
-        # Only look at the training set 3 folder
-        folder = pathlib.Path("dicoms/Training set 3 (base of jaw)")
-        for path in folder.glob("*.dcm"):
-            # Open each DICOM in the training set 3 folder
-            _, mask = io.read_dicom(path)
+    # Only look at the training set 3 folder
+    folder = pathlib.Path("dicoms/Training set 3 (base of jaw)")
+    assert folder.is_dir()
 
-            # Find the last Z slice for which there are at least some nonzero values
-            n_required = 3
-            z_nonzero = np.sum(mask, axis=(1, 2)) > n_required
-            idx = len(z_nonzero) - np.argmax(z_nonzero[::-1])
+    # Get the size of the crop window from the config
+    config = util.userconf()
+    crop_size = transform.window_size(config)
+    for path in folder.glob("*.dcm"):
+        # Open each DICOM in the training set 3 folder
+        _, mask = io.read_dicom(path)
 
-            # Get the size of the crop window from the config
-            config = util.userconf()
-            crop_size = transform.window_size(config)
+        # Find the last Z slice for which there are at least some nonzero values
+        n_required = 3
+        z_nonzero = np.sum(mask, axis=(1, 2)) > n_required
+        idx = len(z_nonzero) - np.argmax(z_nonzero[::-1])
 
-            # If our crop window would overlap with the edge of the image in Z, error
-            # We don't want to fiddle with the Z location since this might
-            # accidentally make the window contain unlabelled jaw
-            if transform.crop_out_of_bounds(
-                *transform.start_and_end(idx, crop_size[0], start_from_loc=True),
-                mask.shape[0],
-            ):
-                raise ValueError(
-                    f"""Z crop out of bounds for {path}:
-                        {transform.start_and_end(idx,
-                                                 crop_size[0],
-                                                 start_from_loc=True,
-                                                 )}
-                        bound for image size {mask.shape[0]}
-                     """
-                )
+        # If our crop window would overlap with the edge of the image in Z, error
+        # We don't want to fiddle with the Z location since this might
+        # accidentally make the window contain unlabelled jaw
+        if transform.crop_out_of_bounds(
+            *transform.start_and_end(idx, crop_size[0], start_from_loc=True),
+            mask.shape[0],
+        ):
+            raise ValueError(
+                f"""Z crop out of bounds for {path}:
+                    {transform.start_and_end(idx,
+                                             crop_size[0],
+                                             start_from_loc=True,
+                                             )}
+                    bound for image size {mask.shape[0]}
+                 """
+            )
 
-            # Find the xy centre of that slice
-            x, y = find_xy(mask[idx - 1])
+        # Find the xy centre of that slice
+        x, y = find_xy(mask[idx - 1])
 
-            # Check if this overlaps with the edge of the image
-            # start_from_loc is False here since we crop centrally around X and Y
-            if transform.crop_out_of_bounds(
-                *transform.start_and_end(x, crop_size[1], start_from_loc=False),
-                mask.shape[1],
-            ):
-                warning_buffer.append(
-                    f"""X crop out of bounds for {path}:
-                        {transform.start_and_end(x, crop_size[1], start_from_loc=False)}
-                        bound for image size {mask.shape[1]}
-                     """
-                )
-                # Move the X location such that it doesn't overlap with the edge
-                # x = mask.shape[1]
-                ...
+        # Check if this overlaps with the edge of the image
+        # start_from_loc is False here since we crop centrally around X and Y
+        if transform.crop_out_of_bounds(
+            *(bounds := transform.start_and_end(x, crop_size[1], start_from_loc=False)),
+            mask.shape[1],
+        ):
+            warning_buffer.append(
+                f"""X crop out of bounds for {path}:
+                    {bounds=}
+                    for image size {mask.shape[1]}
+                 """
+            )
+            print(bounds)
+            # Move the X location such that it doesn't overlap with the edge
+            # x = mask.shape[1]
 
-            if transform.crop_out_of_bounds(
-                *transform.start_and_end(y, crop_size[2], start_from_loc=False),
-                mask.shape[2],
-            ):
-                warning_buffer.append(
-                    f"""Y crop out of bounds for {path}:
-                        {transform.start_and_end(y,
-                                                 crop_size[2],
-                                                 start_from_loc=False,
-                                                 )}
-                        bound for image size {mask.shape[2]}
-                     """
-                )
-                # Move the X location such that it doesn't overlap with the edge
-                # y = mask.shape[2]
-                ...
+        if transform.crop_out_of_bounds(
+            *(bounds := transform.start_and_end(y, crop_size[2], start_from_loc=False)),
+            mask.shape[2],
+        ):
+            warning_buffer.append(
+                f"""Y crop out of bounds for {path}:
+                    {bounds=}
+                    for image size {mask.shape[2]}
+                 """
+            )
+            print(bounds)
+            # Move the X location such that it doesn't overlap with the edge
+            # y = mask.shape[2]
 
-            # Find n from the path
-            n = int(path.stem.split("_", maxsplit=1)[-1])
+        # Find n from the path
+        n = int(path.stem.split("_", maxsplit=1)[-1])
 
-            # Plot them
-            fig, axes = plt.subplots(1, 3, figsize=(12, 4))
-            for i, (axis, img) in enumerate(zip(axes, mask[idx - 2 : idx + 1])):
-                axis.imshow(img)
-                axis.plot(y, x, "ro")
-            fig.savefig(plot_dir / f"{n}.png")
-            plt.close(fig)
+        # Plot them
+        fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+        for i, (axis, img) in enumerate(zip(axes, mask[idx - 2 : idx + 1])):
+            axis.imshow(img)
+            axis.plot(y, x, "ro")
+        fig.savefig(plot_dir / f"{n}.png")
+        plt.close(fig)
 
-            # Output n,z
-            # This is what we'll copy and paste into the csv
-            print(n, idx, round(x), round(y), "FALSE", sep=",")
+        # Output n,z
+        # This is what we'll copy and paste into the csv
+        results_buffer.append(f"{n},{idx},{round(x)},{round(y)},FALSE")
 
-        for message in warning_buffer:
-            warnings.warn(message)
+    # Print the results
+    print("\n".join(results_buffer))
+
+    for message in warning_buffer:
+        warnings.warn(message)
 
 
 if __name__ == "__main__":
