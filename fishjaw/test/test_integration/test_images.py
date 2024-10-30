e137ead871871a56d08ee9d69e0939b8553aeb00
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-10-30 11:27:48 +0000

    unit tests

--- fishjaw/test/test_integration/test_images.py
+++ fishjaw/test/test_integration/test_images.py
@@ -31,7 +31,7 @@ def test_around_centre():
     assert transform.around_centre(30)
     assert transform.around_centre(107)
 
-    assert not transform.around_centre(1)
+    assert not transform.around_centre(25)
 
 
 def test_find_coords():
@@ -138,39 +138,122 @@ def test_crop_larger_than_image():
         transform.crop(np.ones((10, 10, 10)), (5, 5, 5), (11, 10, 10), True)
 
 
-def test_x_overlap(test_img: np.ndarray):
-    """
-    Check the right error is raised
-
-    """
-    crop_size = (4, 4, 4)
-    with pytest.raises(ValueError):
-        transform.crop(test_img, (5, 9, 5), crop_size, True)
-
-    with pytest.raises(ValueError):
-        transform.crop(test_img, (5, 1, 5), crop_size, True)
-
-    with pytest.raises(ValueError):
-        transform.crop(test_img, (5, 9, 5), crop_size, False)
-
-    with pytest.raises(ValueError):
-        transform.crop(test_img, (5, 1, 5), crop_size, False)
-
-
 def test_z_overlap(test_img: np.ndarray):
     """
     Check the right error is raised
 
     """
     crop_size = (4, 4, 4)
-    with pytest.raises(ValueError):
+    with pytest.raises(transform.CropOutOfBoundsError):
         transform.crop(test_img, (1, 5, 5), crop_size, True)
 
-    with pytest.raises(ValueError):
+    with pytest.raises(transform.CropOutOfBoundsError):
         transform.crop(test_img, (9, 5, 5), crop_size, True)
 
-    with pytest.raises(ValueError):
+    with pytest.raises(transform.CropOutOfBoundsError):
         transform.crop(test_img, (1, 5, 5), crop_size, False)
 
-    with pytest.raises(ValueError):
+    with pytest.raises(transform.CropOutOfBoundsError):
         transform.crop(test_img, (11, 5, 5), crop_size, False)
+
+
+def test_x_out_of_bounds_crop(test_img: np.ndarray):
+    """
+    Check the right error is raised
+
+    """
+    crop_size = (4, 4, 4)
+
+    # Crop right up to the edge
+    transform.crop(test_img, (5, 8, 5), crop_size, True)
+    # Too large
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 9, 5), crop_size, True)
+
+    # Crop right from the start
+    transform.crop(test_img, (5, 2, 5), crop_size, True)
+    # Too small
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 1, 5), crop_size, True)
+
+
+def test_x_out_of_bounds_crop_offset(test_img: np.ndarray):
+    """
+    Check the right error is raised
+
+    """
+    crop_size = (4, 4, 4)
+    transform.crop(test_img, (5, 8, 5), crop_size, False)
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 9, 5), crop_size, False)
+    transform.crop(test_img, (5, 2, 5), crop_size, False)
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 1, 5), crop_size, False)
+
+
+def test_y_out_of_bounds_crop(test_img: np.ndarray):
+    """
+    Check the right error is raised
+
+    """
+    crop_size = (4, 4, 4)
+
+    # Crop right up to the edge
+    transform.crop(test_img, (5, 5, 8), crop_size, True)
+    # Too large
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 5, 9), crop_size, True)
+
+    # Crop right from the start
+    transform.crop(test_img, (5, 5, 2), crop_size, True)
+    # Too small
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 5, 1), crop_size, True)
+
+
+def test_y_out_of_bounds_crop_offset(test_img: np.ndarray):
+    """
+    Check the right error is raised
+
+    """
+    crop_size = (4, 4, 4)
+    transform.crop(test_img, (5, 5, 8), crop_size, False)
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 5, 9), crop_size, False)
+    transform.crop(test_img, (5, 5, 2), crop_size, False)
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (5, 5, 1), crop_size, False)
+
+
+def test_z_out_of_bounds_crop(test_img: np.ndarray):
+    """
+    Check the right error is raised
+
+    """
+    crop_size = (4, 4, 4)
+
+    # Crop right up to the edge
+    transform.crop(test_img, (8, 5, 5), crop_size, True)
+    # Too large
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (9, 5, 5), crop_size, True)
+
+    # Crop right from the start
+    transform.crop(test_img, (2, 5, 5), crop_size, True)
+    # Too small
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (1, 5, 5), crop_size, True)
+
+
+def test_z_out_of_bounds_crop_offset(test_img: np.ndarray):
+    """
+    Check the right error is raised
+
+    """
+    crop_size = (4, 4, 4)
+    transform.crop(test_img, (10, 5, 5), crop_size, False)
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (11, 5, 5), crop_size, False)
+    transform.crop(test_img, (4, 5, 5), crop_size, False)
+    with pytest.raises(transform.CropOutOfBoundsError):
+        transform.crop(test_img, (3, 5, 5), crop_size, False)
