83460219af0cc2241e7e38a254dfd52cee33a310
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-11-01 10:48:05 +0000

    find commit time

--- scripts/lines_of_code.py
+++ scripts/lines_of_code.py
@@ -5,9 +5,24 @@ Creates lines_of_code.png in the script output directory.
 
 """
 
+import datetime
 import subprocess
 
 from fishjaw.util import files
+
+
+def _get_commit_time(commit: str) -> datetime.datetime:
+    """
+    Get the time at which a commit was made (UTC)
+
+    """
+    commit_info = subprocess.run(
+        ["git", "cat-file", "commit", commit], capture_output=True, check=True
+    ).stdout.decode("utf-8")
+    for line in commit_info.split("\n"):
+        if line.startswith("committer"):
+            time = line.split(" ")[-2]
+            return datetime.datetime.fromtimestamp(int(time), tz=datetime.timezone.utc)
 
 
 def main():
@@ -19,9 +34,13 @@ def main():
         subprocess.run(["git", "rev-list", "main"], capture_output=True, check=True)
         .stdout.decode("utf-8")
         .split("\n")
-    )[:-1]  # The last element is an empty string
+    )[
+        :-1  # The last element is an empty string
+    ]
 
-    # Get the hashes from main
+    for commit in commits:
+        time = _get_commit_time(commit)
+
     # Find the date
     # Find all python files for each
     # Count the lines of code
