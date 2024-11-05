4d2236ce87b358bc52be1f1ff9ee2f18fe6d793f
richard-lane <56296326+richard-lane@users.noreply.github.com>
2024-11-05 16:46:48 +0000

    Feature/jaw occupancy (#23)
    Plot the jaw occupancy (~size?) against fish age. The results may surprise you
    ---------
    
    Co-authored-by: Richard Lane <mh19137@it090783.users.bris.ac.uk>

--- scripts/find_occupancy.py
+++ scripts/find_occupancy.py
@@ -8,9 +8,52 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-from fishjaw.util import util
-from fishjaw.util import files
 from fishjaw.images import io, transform
+from fishjaw.util import util, files, metadata
+
+
+def _hist(cropped_means: list[float]) -> None:
+    """
+    Make a histogram of the cropped means
+
+    """
+    fig, axis = plt.subplots()
+
+    axis.hist(cropped_means)
+    axis.set_xlabel("Occupancy (\u2030)")
+    axis.set_ylabel("Frequency")
+    axis.set_title("Histogram jaw occupancy after cropping")
+
+    fig.savefig(f"{files.script_out_dir()}/jaw_occupancy.png")
+    plt.close(fig)
+
+
+def _scatter(cropped_means, ages) -> None:
+    """
+    Make plots of mean size against age
+
+    """
+    fig, axis = plt.subplots()
+
+    axis.plot(ages, cropped_means, "o")
+
+    pts = axis.get_xlim()
+
+    def line(x, a, b):
+        """Straight line"""
+        return a * x + b
+
+    popt = np.polyfit(ages, cropped_means, 1)
+    axis.plot(pts, [line(pt, *popt) for pt in pts], "r")
+    axis.set_xlim(pts)
+
+    axis.set_title("Cropped Jaw Occupancy vs Age")
+
+    axis.set_xlabel("Age (months)")
+    axis.set_ylabel("Occupancy (\u2030)")
+
+    fig.tight_layout()
+    fig.savefig(f"{files.script_out_dir()}/jaw_occupancy_age.png")
 
 
 def main():
@@ -24,14 +67,20 @@ def main():
     # Don't need to pass a config in if we're reading all the DICOMs
     paths = files.dicom_paths(None, "all")
 
+    # We only want the complete jaws
+    paths = [path for path in paths if "Training set 3 (base of jaw)" not in str(path)]
+
     means = []
     cropped_means = []
+    ages = []
     for path in tqdm(paths):
         _, mask = io.read_dicom(path)
 
         n = int(path.stem.split("_", maxsplit=1)[-1])
         crop_coords = transform.centre(n)
         around_centre = transform.around_centre(n)
+
+        ages.append(metadata.age(n))
 
         means.append(1000 * mask.mean())
 
@@ -44,11 +93,8 @@ def main():
         f"Cropped: {np.mean(cropped_means):.2f} +/- {np.std(cropped_means):.2f}\u2030"
     )
 
-    plt.hist(cropped_means)
-    plt.xlabel("Occupancy (\u2030)")
-    plt.ylabel("Frequency")
-    plt.title("Histogram jaw occupancy after cropping")
-    plt.savefig(f"{files.script_out_dir()}/jaw_occupancy.png")
+    _hist(cropped_means)
+    _scatter(cropped_means, ages)
 
 
 if __name__ == "__main__":
