5e5e129bd50de662c9811e53ace583f8786a0613
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-11-08 15:30:05 +0000

    fpr

--- fishjaw/images/metrics.py
+++ fishjaw/images/metrics.py
@@ -3,7 +3,26 @@ Metrics for evaluating similarity etc. between images.
 
 """
 
+import warnings
+
 import numpy as np
+
+
+def _check_arrays(truth: np.ndarray, pred: np.ndarray) -> None:
+    """
+    Check the arrays are the right shape and formats
+
+    """
+    if truth.shape != pred.shape:
+        raise ValueError(
+            f"Shapes of truth {truth.shape} and pred {pred.shape} arrays do not match"
+        )
+    if set(np.unique(truth)) - {0, 1}:
+        raise ValueError(f"truth array is not binary: {np.unique(truth)=}")
+    if not (0 <= pred).all() and (pred <= 1).all():
+        raise ValueError(
+            f"pred array is not in the range [0, 1]: {np.min(pred)=}, {np.max(pred)=}"
+        )
 
 
 def dice_score(truth: np.ndarray, pred: np.ndarray) -> float:
@@ -19,16 +38,7 @@ def dice_score(truth: np.ndarray, pred: np.ndarray) -> float:
     :raises: ValueError if the pred array is not in the range [0, 1].
 
     """
-    if truth.shape != pred.shape:
-        raise ValueError(
-            f"Shapes of truth {truth.shape} and pred {pred.shape} arrays do not match"
-        )
-    if set(np.unique(truth)) - {0, 1}:
-        raise ValueError(f"truth array is not binary: {np.unique(truth)=}")
-    if not (0 <= pred).all() and (pred <= 1).all():
-        raise ValueError(
-            f"pred array is not in the range [0, 1]: {np.min(pred)=}, {np.max(pred)=}"
-        )
+    _check_arrays(truth, pred)
 
     intersection = np.sum(truth * pred)
     volume1 = np.sum(truth)
@@ -36,6 +46,35 @@ def dice_score(truth: np.ndarray, pred: np.ndarray) -> float:
 
     # Both arrays are empty, consider Dice score as 1
     if volume1 + volume2 == 0:
+        warnings.warn("Both arrays are empty, returning Dice score of 1")
         return 1.0
 
     return 2.0 * intersection / (volume1 + volume2)
+
+
+def fpr(truth: np.ndarray, pred: np.ndarray) -> float:
+    """
+    Calculate the false positive rate between a binary mask (truth) and a float array (pred).
+
+
+
+    :param truth: Binary mask array.
+    :param pred: Float prediction array.
+
+    :returns: False positive rate.
+    :raises: ValueError if the shapes of the arrays do not match.
+    :raises: ValueError if the truth array is not binary.
+    :raises: ValueError if the pred array is not in the range [0, 1].
+
+    """
+    _check_arrays(truth, pred)
+
+    negatives = np.sum(truth == 0)
+
+    if negatives == 0:
+        warnings.warn("Both arrays are empty, returning FPR of 0")
+        return 0.0
+
+    weighted_false_positives = np.sum(~truth * pred)
+
+    return weighted_false_positives / negatives
