7381b6b704560870f9aef5c49c06f7493ac65f71
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-11-19 13:19:26 +0000

    plot voxels

--- scripts/boring_scripts/voxels.py
+++ scripts/boring_scripts/voxels.py
@@ -21,7 +21,8 @@ def main(args: argparse.Namespace):
 
     """
     # Read the labels
-    labels = tifffile.imread(args.label_path)[::4, ::4, ::4]
+    label_path = pathlib.Path(args.label_path)
+    labels = tifffile.imread(label_path)
 
     # Find the unique labels
     unique_labels = np.unique(labels)
@@ -33,27 +34,41 @@ def main(args: argparse.Namespace):
 
     colours = np.empty(labels.shape, dtype=object)
     for label in unique_labels:
-        # Ignore the background
-        if not label:
-            continue
-
         colours[labels == label] = hex_colors[label]
 
     # Plot the voxels
     fig, axes = plt.subplots(1, 3, figsize=(15, 10), subplot_kw={"projection": "3d"})
-    for axis, elev, azim in tqdm(zip(axes, [0, 90, 0], [0, 0, 90]), total=3):
-        axis.voxels(labels, facecolors=colours, edgecolors="k")
+    pbar = tqdm(total=3 * (len(unique_labels) - 1))
+    for axis, elev, azim in zip(axes, [0, 90, 0], [0, 0, 90]):
+        for label in unique_labels:
+            # Don't plot the background
+            if not label:
+                continue
+
+            # Find co-ords
+            co_ords = np.argwhere(labels == label)
+
+            axis.scatter(
+                co_ords[:, 0], co_ords[:, 1], co_ords[:, 2], c=hex_colors[label], s=1
+            )
+
+            pbar.update(1)
+
         axis.view_init(elev=elev, azim=azim)
 
         axis.set_xticks([])
         axis.set_yticks([])
         axis.set_zticks([])
 
-    path = (
-        files.boring_script_out_dir()
-        / f"{pathlib.Path(args.label_path).stem}/_voxels.png"
+    fig.suptitle(label_path.stem)
+    fig.tight_layout()
+
+    out_path = (
+        files.boring_script_out_dir() / "voxels" / f"{label_path.stem}_voxels.png"
     )
-    fig.savefig(path)
+    if not out_path.parent.exists():
+        out_path.parent.mkdir(parents=True)
+    fig.savefig(out_path)
 
 
 if __name__ == "__main__":
