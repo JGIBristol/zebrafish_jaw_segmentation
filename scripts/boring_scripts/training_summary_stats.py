ab55205580de422231329079aaca4fe9086a502f
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-12-10 18:19:49 +0000

    print the stuff we want

--- scripts/boring_scripts/training_summary_stats.py
+++ scripts/boring_scripts/training_summary_stats.py
@@ -31,17 +31,23 @@ def _print_info(label: str, n: list[int], fish_info: pd.DataFrame) -> None:
     :param fish_info: DataFrame with fish info
 
     """
-    df_slice = fish_info[["age"]].loc[n]
+    df_slice = fish_info[["age", "genotype", "strain"]].loc[n]
+    end_str = f"{'=' * 80}\n"
 
     print(f"Summary stats for {label} set")
     if label in {"val", "test"}:
-        print(df_slice.to_markdown(), end=f"\n{'='*24}\n\n")
+        print(df_slice.to_markdown())
+        print(end_str)
 
     else:
         # For train set, Find average and 95% CI for age
+        print(f"N: {len(n)}")
         print(
-            f"\tAverage age: {df_slice['age'].mean():.2f} mo [95% CI: {df_slice['age'].quantile([0.025, 0.975]).values}]"
+            f"Average age: {df_slice['age'].mean():.2f} mo [95% CI: {df_slice['age'].quantile([0.025, 0.975]).values}]"
         )
+        print(df_slice.groupby("age").size().sort_index(ascending=True).to_markdown())
+        print(df_slice.groupby(["genotype", "strain"]).size().sort_values(ascending=False).to_markdown())
+        print(end_str)
 
 
 def main():
