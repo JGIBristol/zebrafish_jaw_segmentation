69624e0ea3929c366a2102e06c2df2235702050d
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-12-10 11:07:09 +0000

    skel

---
+++ scripts/boring_scripts/training_summary_stats.py
@@ -0,0 +1,16 @@
+"""
+Plot summary stats for the training, testing and validation data
+
+"""
+
+
+def main():
+    """
+    Get the ID numbers of the fish used for training, testing and validation; print the average age and
+    their mutations
+
+    """
+
+
+if __name__ == "__main__":
+    main()
