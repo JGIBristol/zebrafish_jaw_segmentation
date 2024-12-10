afd7928cece154c1c4b22491e647e5363c6d08db
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-12-10 11:23:43 +0000

    more stuff

--- scripts/boring_scripts/training_summary_stats.py
+++ scripts/boring_scripts/training_summary_stats.py
@@ -2,6 +2,18 @@
 Plot summary stats for the training, testing and validation data
 
 """
+import pathlib
+
+from fishjaw.util import files, util
+
+
+def _n_from_paths(paths: list[pathlib.Path]) -> int:
+    """
+    Get the number of fish from the paths
+
+    :param paths: list of paths
+    :returns: number of fish
+    """
 
 
 def main():
@@ -10,6 +22,19 @@ def main():
     their mutations
 
     """
+    # Read config
+    config = util.userconf()
+
+    val_files = files.dicom_paths(config, "val")
+    test_files = files.dicom_paths(config, "test")
+    train_files = files.dicom_paths(config, "train")
+
+    # Get n from
+    # files.dicom_paths(config: dict[str, Any], mode: str) -> list[pathlib.Path]:
+    # For val set, just print the rows
+    # For test set, print the row
+    # For train set, Find average and 95% CI for age
+    # Value counts for mutations
 
 
 if __name__ == "__main__":
