66c11b61bf15b43f9a0514a4b8dda5b2be6cbb29
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-12-10 17:29:42 +0000

    print stats

--- scripts/boring_scripts/training_summary_stats.py
+++ scripts/boring_scripts/training_summary_stats.py
@@ -5,7 +5,11 @@ Plot summary stats for the training, testing and validation data
 
 import pathlib
 
+import pandas as pd
+import prettytable
+
 from fishjaw.util import files, util
+from fishjaw.images.transform import jaw_centres
 
 
 def _n_from_paths(paths: list[pathlib.Path]) -> list[int]:
@@ -16,7 +20,28 @@ def _n_from_paths(paths: list[pathlib.Path]) -> list[int]:
     :returns: number of fish
 
     """
-    return [path.stem for path in paths]
+    return [int(path.stem) for path in paths]
+
+
+def _print_info(label: str, n: list[int], fish_info: pd.DataFrame) -> None:
+    """
+    Print the fish info
+
+    :param n: list of fish numbers
+    :param fish_info: DataFrame with fish info
+
+    """
+    df_slice = fish_info[["age"]].loc[n]
+
+    print(f"Summary stats for {label} set")
+    if label in {"val", "test"}:
+        print(df_slice.to_markdown(), end=f"\n{'='*24}\n\n")
+
+    else:
+        # For train set, Find average and 95% CI for age
+        print(
+            f"\tAverage age: {df_slice['age'].mean():.2f} mo [95% CI: {df_slice['age'].quantile([0.025, 0.975]).values}]"
+        )
 
 
 def main():
@@ -27,14 +52,17 @@ def main():
     """
     # Read config
     config = util.userconf()
+    fish_info = jaw_centres()
 
-    val_files, test_files, train_files = (
-        files.dicom_paths(config, mode) for mode in ["val", "test", "train"]
+    val, test, train = (
+        _n_from_paths(files.dicom_paths(config, mode))
+        for mode in ["val", "test", "train"]
     )
 
-    # For val set, just print the rows
-    # For test set, print the row
-    # For train set, Find average and 95% CI for age
+    _print_info("val", val, fish_info)
+    _print_info("test", test, fish_info)
+    _print_info("train", train, fish_info)
+
     # Value counts for mutations
 
 
