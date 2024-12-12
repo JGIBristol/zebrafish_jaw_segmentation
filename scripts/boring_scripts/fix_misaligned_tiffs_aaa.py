5dab5cf9a7ebe6b6bb0d4ef2a6789d87bd586cff
Richard Lane <richard.lane@bristol.ac.uk>
2024-12-12 11:34:23 +0000

    some script changes

--- scripts/boring_scripts/fix_misaligned_tiffs_aaa.py
+++ scripts/boring_scripts/fix_misaligned_tiffs_aaa.py
@@ -24,22 +24,13 @@ def rotations24(polycube, prefix):
         for i in range(4):
             yield f"{prefix}{axes=}_{i=}", np.rot90(polycube, i, axes)
 
-    # 4 rotations about axis 0
+    yield from rotations4(polycube, (0, 1))
+    yield from rotations4(polycube, (0, 2))
     yield from rotations4(polycube, (1, 2))
 
-    # rotate 180 about axis 1, now shape is pointing down in axis 0
-    # 4 rotations about axis 0
-    yield from rotations4(np.rot90(polycube, 2, axes=(0, 2)), (1, 2))
-
-    # rotate 90 or 270 about axis 1, now shape is pointing in axis 2
-    # 8 rotations about axis 2
-    yield from rotations4(np.rot90(polycube, axes=(0, 2)), (0, 1))
-    yield from rotations4(np.rot90(polycube, -1, axes=(0, 2)), (0, 1))
-
-    # rotate about axis 2, now shape is pointing in axis 1
-    # 8 rotations about axis 1
-    yield from rotations4(np.rot90(polycube, axes=(0, 1)), (0, 2))
-    yield from rotations4(np.rot90(polycube, -1, axes=(0, 1)), (0, 2))
+    yield from rotations4(np.rot90(polycube, 1, axes=(0, 1)), (0, 2))
+    yield from rotations4(np.rot90(polycube, 2, axes=(0, 1)), (0, 2))
+    yield from rotations4(np.rot90(polycube, 3, axes=(0, 1)), (0, 2))
 
 
 def rotations48(polycube, prefix):
