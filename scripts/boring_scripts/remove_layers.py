c27d899c4e45758cea650ceab73d39de3896696a
Richard Lane <richard.lane@bristol.ac.uk>
2025-01-13 16:58:44 +0000

    skel

---
+++ scripts/boring_scripts/remove_layers.py
@@ -0,0 +1,17 @@
+"""
+Investigate the effect of removing layers and/or skip connections on the segmentation
+
+"""
+
+import argparse
+
+
+def main():
+    """
+    Load in a model and the data, then evaluate with and without each layer/skip connection
+
+    """
+
+
+if __name__ == "__main__":
+    main()
