be31392a4ef9ca894d6a41472dbef4c85a218371
Richard Lane <richard.lane@bristol.ac.uk>
2025-02-05 11:51:05 +0000

    broken script that compares segmentations

--- scripts/compare_segmentations.py
+++ scripts/compare_segmentations.py
@@ -18,34 +18,7 @@ from fishjaw.model import model, data
 from fishjaw.images import metrics, transform
 from fishjaw.visualisation import images_3d, plot_meshes
 from fishjaw.inference import read, mesh
-
-
-def _save_mesh(
-    segmentation: np.ndarray,
-    subject_name: str,
-    threshold: float,
-    out_dir: pathlib.Path,
-) -> None:
-    """
-    Turn a segmentation into a mesh and save it
-
-    """
-    # Save as STL
-    stl_mesh = mesh.cubic_mesh(segmentation > threshold)
-
-    stl_mesh.save(f"inference/{subject_name}_mesh_{threshold:.3f}.stl")
-
-    # Save projections
-    fig, axes = plt.subplots(1, 3, subplot_kw={"projection": "3d"}, figsize=(15, 5))
-    plot_meshes.projections(axes, stl_mesh, plot_kw={"alpha": 0.4, "cmap": "cividis_r"})
-
-    fig.tight_layout()
-    fig.savefig(
-        f"{out_dir}/{subject_name}_mesh_{threshold:.3f}_projections.png",
-        bbox_inches="tight",
-        pad_inches=0,
-    )
-    plt.close(fig)
+from fishjaw.util import util
 
 
 def _save_test_meshes(
@@ -94,90 +67,6 @@ def _save_test_meshes(
     plt.close(fig)
 
 
-def _make_plots(
-    args: argparse.Namespace,
-    net: torch.nn.Module,
-    subject: tio.Subject,
-    config: dict,
-    activation: str,
-    batch_size: int = 1,
-) -> None:
-    """
-    Make the inference plots using a model and subject
-
-    """
-    # Create the output dir
-    out_dir, _ = args.model_name.split(".pkl")
-    assert not _, "Model name should end with .pkl"
-
-    # Append _speckle if we're removing voxels
-    if args.speckle:
-        out_dir += "_speckle"
-    if args.splotch:
-        out_dir += "_splotch"
-
-    out_dir = files.script_out_dir() / "inference" / out_dir
-    if not out_dir.exists():
-        out_dir.mkdir(parents=True)
-
-    # Perform inference
-    print("Performing inference")
-
-    # Remove every second voxel to make the segmentation worse
-    if args.speckle:
-        prediction[::2, ::2, ::2] = 0
-        prediction[1::2, 1::2, ::2] = 0
-        prediction[::2, 1::2, 1::2] = 0
-        prediction[1::2, ::2, 1::2] = 0
-
-    if args.splotch:
-        prediction = _add_random_blobs(args.rng, prediction)
-
-    # Convert the image to a 3d numpy array - for plotting
-    image = subject[tio.IMAGE][tio.DATA].squeeze().numpy()
-
-    # Save the image and prediction as tiffs
-    prefix = args.subject if args.subject else "test"
-    tifffile.imwrite(out_dir / f"{prefix}_image.tif", image)
-    tifffile.imwrite(out_dir / f"{prefix}_prediction.tif", prediction)
-
-    # Save the output image and prediction as slices
-    fig, _ = images_3d.plot_slices(image, prediction)
-
-    # If we're using the test data, we have access to the ground truth so can
-    # work out the Dice score and stick it in the plot too
-    if args.test:
-        truth = subject[tio.LABEL][tio.DATA].squeeze().numpy()
-        dice = metrics.dice_score(truth, prediction)
-        fig.suptitle(f"Dice: {dice:.3f}", y=0.99)
-
-        # We might as well save the truth as a tiff too
-        tifffile.imwrite(out_dir / f"{prefix}_truth.tif", truth)
-
-        # Print a table of metrics
-        print(
-            metrics.table([truth], [prediction], thresholded_metrics=True).to_markdown()
-        )
-
-    else:
-        fig.suptitle(f"Inference: ID {args.subject}", y=0.99)
-
-    fig.savefig(out_dir / f"{prefix}_slices.png")
-    plt.close(fig)
-
-    # Save the mesh
-    if args.mesh:
-        threshold = 0.5
-        print("Saving predicted mesh")
-        _save_mesh(prediction, prefix, threshold, out_dir)
-
-        # Save the mesh on top of the ground truth
-        if args.test:
-            print("Saving test meshes")
-            thresholded = prediction > threshold
-            _save_test_meshes(thresholded, truth, out_dir)
-
-
 def _inference(model_name: str) -> NDArray:
     """
     Do the inference, save the plots
@@ -219,18 +108,41 @@ def main(*, model_name: str):
         out_dir.mkdir(parents=True)
 
     # Perform inference with the model
+    print("Performing inference")
     inference = _inference(model_name)
 
     # Load the human segmentations
+    print("loading human segmentations")
+    config = util.userconf()
+    seg_dir = (
+        files.rdsf_dir(config)
+        / "1Felix and Rich make models"
+        / "Human validation STL and results"
+    )
+    felix = tifffile.imread(seg_dir / "Felix" / "ak_97.tif.labels(2).tif")
+    harry = tifffile.imread(seg_dir / "Harry" / "ak_97.tif.labels.tif")
+    tahlia = tifffile.imread(seg_dir / "Tahlia" / "tpollock_97_avizo.labels.tif")
 
     # Save the inference as a tiff
+    print("Saving the inference")
     tifffile.imwrite(out_dir / "inference.tif", inference)
 
     # Modify the model segmentation to make it worse
-    speckled = transform.speckle(inference, rng)
-    splotched = transform.add_random_blobs(rng, speckled)
+    print("Modifying the model segmentation")
+    speckled = transform.speckle(inference)
+    splotched = transform.add_random_blobs(rng, inference)
+    tifffile.imwrite(out_dir / "speckled.tif", speckled)
+    tifffile.imwrite(out_dir / "splotched.tif", splotched)
 
     # Compare the segmentations to a baseline, print a table of metrics
+    print("Comparing segmentations")
+    print(
+        metrics.table(
+            [felix] * 5,
+            [felix, harry, tahlia, inference, speckled, splotched],
+            thresholded_metrics=True,
+        )
+    )
 
 
 if __name__ == "__main__":
