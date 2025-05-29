8cd35a4b6f719fbb67a0b16c74153644aefd01f7
Richard Lane <richard.lane@bristol.ac.uk>
2025-05-29 16:14:56 +0100

    dict for quadrate paths

--- fishjaw/transfer/data.py
+++ fishjaw/transfer/data.py
@@ -2,14 +2,34 @@
 Data for transfer learning
 """
 
+import re
+import pathlib
+
 import torchio as tio
 
+from fishjaw.util import files
 
-def _quadrate_paths() -> dict[str, str]:
+
+def _quadrate_paths(config: dict) -> dict[pathlib.Path, pathlib.Path]:
     """
     Return a mapping from the quadrate data paths to their respective labels
     on the RDSF.
+
+    :param config: from userconf.yml
     """
+    labels = (
+        config["rdsf_dir"]
+        / pathlib.Path(
+            "1Felix and Rich make models/Training dataset Tiffs/Training set 1"
+        )
+    ).glob("*.tif")
+
+    # Remove these ones, since the 3D tifs dont exist
+    bad_labels = re.compile(r"(351|401|420|441)")
+    labels = [label for label in labels if not bad_labels.search(label.name)]
+    imgs = [files.get_3d_tif(label_path) for label_path in labels]
+
+    return dict(zip(labels, imgs, strict=True))
 
 
 def _cache_quadrates() -> None:
