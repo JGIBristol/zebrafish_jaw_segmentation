2f43e574a60c0572dcb5fd001da16eb165882f7c
Richard Lane <richard.lane@bristol.ac.uk>
2025-07-31 14:40:43 +0100

    skel for downsample fcn

--- fishjaw/localisation/data.py
+++ fishjaw/localisation/data.py
@@ -5,6 +5,8 @@ Training data for the jaw localisation model.
 
 import pathlib
 
+import numpy as np
+
 
 def downsampled_dicom_path(dicom_path: pathlib.Path) -> pathlib.Path:
     """
@@ -15,3 +17,11 @@ def downsampled_dicom_path(dicom_path: pathlib.Path) -> pathlib.Path:
     i = parts.index("dicoms")
     downsampled_parts = parts[: i + 1] + ("downsampled_dicoms",) + parts[i + 1 :]
     return pathlib.Path(*downsampled_parts)
+
+
+def downsampled_imgs(
+    image: np.ndarray, mask: np.ndarray, target_shape: tuple[int, int, int]
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Downsample the image and mask to the provided target shape
+    """
