2517b0663740a923bd69e2e63f0ff0e1f4db18a4
Richard Lane <richard.lane@bristol.ac.uk>
2025-08-07 14:44:42 +0100

    fcn to plot on axis

--- fishjaw/visualisation/training.py
+++ fishjaw/visualisation/training.py
@@ -8,6 +8,30 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
+def plot_loss_axis(
+    axis: plt.Axes, train_losses: list[list[float]], val_losses: list[list[float]]
+) -> None:
+    """
+    Plot on an axis
+    """
+    axis.plot(epochs, train_loss, label="Train")
+
+    # Find quartiles - the mean might be outside this, which would be interesting wouldn't it
+    train_loss_upper = [np.percentile(epoch_loss, 75) for epoch_loss in train_losses]
+    train_loss_lower = [np.percentile(epoch_loss, 25) for epoch_loss in train_losses]
+    axis.fill_between(epochs, train_loss_lower, train_loss_upper, alpha=0.5, color="C0")
+
+    axis.plot(epochs, val_loss, label="Validation")
+
+    val_loss_upper = [np.percentile(epoch_loss, 75) for epoch_loss in val_losses]
+    val_loss_lower = [np.percentile(epoch_loss, 25) for epoch_loss in val_losses]
+    axis.fill_between(epochs, val_loss_lower, val_loss_upper, alpha=0.5, color="C1")
+
+    axis.set_title("Loss")
+    axis.set_xlabel("Epoch")
+    axis.legend()
+
+
 def plot_losses(
     train_losses: list[list[float]], val_losses: list[list[float]]
 ) -> matplotlib.figure.Figure:
@@ -26,22 +50,7 @@ def plot_losses(
 
     fig, axis = plt.subplots()
 
-    axis.plot(epochs, train_loss, label="Train")
-
-    # Find quartiles - the mean might be outside this, which would be interesting wouldn't it
-    train_loss_upper = [np.percentile(epoch_loss, 75) for epoch_loss in train_losses]
-    train_loss_lower = [np.percentile(epoch_loss, 25) for epoch_loss in train_losses]
-    axis.fill_between(epochs, train_loss_lower, train_loss_upper, alpha=0.5, color="C0")
-
-    axis.plot(epochs, val_loss, label="Validation")
-
-    val_loss_upper = [np.percentile(epoch_loss, 75) for epoch_loss in val_losses]
-    val_loss_lower = [np.percentile(epoch_loss, 25) for epoch_loss in val_losses]
-    axis.fill_between(epochs, val_loss_lower, val_loss_upper, alpha=0.5, color="C1")
-
-    axis.set_title("Loss")
-    axis.set_xlabel("Epoch")
-    axis.legend()
+    plot_loss_axis(axis, train_losses, val_losses)
 
     fig.tight_layout()
     return fig
