f6915c2e72e6eeb6793f487ff60c8ff5de3dfc2f
Richard Lane <richard.lane@bristol.ac.uk>
2025-08-13 10:10:11 +0100

    segment jaw script

---
+++ scripts/pipeline/segment_jaws.py
@@ -0,0 +1,74 @@
+"""
+Read in full-size CT scans from Wahab's 3D TIF directory, locate the
+jaw and segment it out
+
+Saves the cropped jaw TIF and segmentation mask
+"""
+
+import pathlib
+import argparse
+import tifffile
+
+from tqdm import tqdm
+
+from fishjaw.util import files, util
+from fishjaw.inference import models
+
+
+def main(crop_size: int):
+    """
+    Get a list of the files in Wahab's dir, load in the two models,
+    iterate over the files cropping + performing inference
+
+    Then save each for later analysis
+    """
+    out_dir = files.script_out_dir() / "jaw_segmentations"
+
+    img_out_dir = out_dir / "imgs"
+    mask_out_dir = out_dir / "masks"
+
+    img_out_dir.mkdir(parents=True, exist_ok=True)
+    mask_out_dir.mkdir(parents=True, exist_ok=True)
+
+    # Get the input dir
+    config = util.userconf()
+    input_dir = (
+        pathlib.Path(config["rdsf_dir"])
+        / "DATABASE"
+        / "uCT"
+        / "Wahab_clean_dataset"
+        / "TIFS"
+    )
+
+    # Get the models
+    device = config["device"]
+    loc_model = models.get_jaw_loc_model(device=device)
+    seg_model = models.get_jaw_segment_model(device=device)
+
+    for img_path in tqdm(sorted(list(input_dir.glob("*.tif")))):
+        name = img_path.name
+        scan = tifffile.imread(img_path)
+
+        # Crop the image
+        cropped = models.crop_jaw(loc_model, scan, window_size=tuple([crop_size] * 3))
+
+        # Run inference
+        prediction = models.segment_jaw(cropped, seg_model)
+
+        # Save
+        tifffile.imwrite(img_out_dir / name, cropped)
+        tifffile.imwrite(mask_out_dir / name, prediction)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--crop-size",
+        type=int,
+        default=192,
+        help="Size of region (in px) to crop around the predicted jaw centre",
+    )
+
+    args = parser.parse_args()
+
+    main(**vars(args))
