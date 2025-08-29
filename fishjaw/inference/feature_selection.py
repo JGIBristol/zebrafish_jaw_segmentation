4b849870e3d09cf4e2d6e5166f9bcfe58c35725b
Richard Lane <richard.lane@bristol.ac.uk>
2025-08-29 14:01:21 +0100

    multiindex

--- fishjaw/inference/feature_selection.py
+++ fishjaw/inference/feature_selection.py
@@ -13,6 +13,8 @@ def add_metadata_cols(feature_df: pd.DataFrame) -> pd.DataFrame:
 
     Adds metadata including fish age, length and mutation status
     (wt/het/hom/mosaic). Assumes the dataframe is indexed by fish number.
+    Adds the new columns in a multi-index labelled "Metadata"; the other
+    columns are in a multi-index named "Features".
 
     :param feature_df: features, indexed by fish number
     :returns: the same dataframe, with metadata columns added to the right
@@ -28,5 +30,11 @@ def add_metadata_cols(feature_df: pd.DataFrame) -> pd.DataFrame:
         metadata_df.loc[n, "age"] = meta.age
         metadata_df.loc[n, "length"] = meta.length
         metadata_df.loc[n, "genotype"] = meta.genotype
+
+    # Add these in a multiindex
+    metadata_df.columns = pd.MultiIndex.from_product(
+        [["Metadata"], metadata_df.columns]
+    )
+    feature_df.columns = pd.MultiIndex.from_product([["Features"], feature_df.columns])
 
     return pd.concat([feature_df, metadata_df], axis=1)
